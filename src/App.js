1d0ad7c5b4d924c27d2b36dbd5619e835f071eac
arturmh <59830180+arturmh@users.noreply.github.com>
2022-04-09 23:05:17 +0200

    Estils basics, NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Board } from './components/Board/Board';
+import { CustomNav } from './components/CustomNav';
 import { Home } from './components/Home';
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Board />} />
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
+    <>
+      <BrowserRouter>
+        <CustomNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Board />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
 
-      </Routes>
-    </BrowserRouter>
 
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { Button, Container } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { Board } from './Board/Board';
 export function Home() {
+  const navigate = useNavigate();
+  const handleNav = (path) => () => navigate(path)
+
   return (
     <>
-      <h1>Escacs</h1>
-      <Link to="/game"><button>Jugar</button></Link>
+
+      <Container className='p-4 mt-5 rounded-3 bg-secondary d-flex' >
+        <div>
+          <h1>Escacs</h1>
+          <Button size='lg' onClick={handleNav('game')}>Jugar</Button>
+        </div>
+        <Board />
+      </Container>
     </>
-    
   )
 }
+
