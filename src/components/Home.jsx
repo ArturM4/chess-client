9719d83a7ba41490f3ae629ccd647682cb17625c
arturmh <59830180+arturmh@users.noreply.github.com>
2022-04-12 00:08:51 +0200

    Responsive home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Col, Container, Row, Stack } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { Board } from './Board/Board';
 export function Home() {
@@ -9,12 +9,19 @@ export function Home() {
   return (
     <>
 
-      <Container className='p-4 mt-5 rounded-3 bg-secondary d-flex' >
-        <div>
-          <h1>Escacs</h1>
-          <Button size='lg' onClick={handleNav('game')}>Jugar</Button>
-        </div>
-        <Board />
+      <Container className='p-4 mt-5 rounded-3 bg-secondary' >
+        <Row>
+          <Col xs={3}>
+            <div>
+              <h1 className='mb-4 mt-3 text-light text-center'>Escacs</h1>
+              <Button size='lg' onClick={handleNav('game')}>Un Jugador</Button>
+
+            </div>
+          </Col>
+          <Col xs={9}>
+            <Board />
+          </Col>
+        </Row>
       </Container>
     </>
   )
