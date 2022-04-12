b836e5b574db45febe26233e2e195a6571c1165a
arturmh <59830180+arturmh@users.noreply.github.com>
2022-04-12 16:56:05 +0200

    add button personalization, responsive margin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Col, Container, Row, Stack } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { Board } from './Board/Board';
+import "../general.css"
 export function Home() {
   const navigate = useNavigate();
   const handleNav = (path) => () => navigate(path)
@@ -16,9 +17,9 @@ export function Home() {
               <h1 className='mb-4 mt-3 text-light text-center'>Escacs</h1>
 
               <Stack className="mx-auto">
-                <Button size='lg' className='ms-2 py-3 mb-2 mb-md-4 ' onClick={handleNav('game')}>Un Jugador</Button>
-                <Button size='lg' className='ms-2 py-3 mb-2 mb-md-4 ' onClick={handleNav('game')}>Multijugador</Button>
-                <Button size='lg' className='ms-2 py-3' onClick={handleNav('game')}>Canviar estils</Button>
+                <Button size='lg' className='c-386ecf ms-2 py-3 mb-2 mb-md-4' onClick={handleNav('game')}>Un Jugador</Button>
+                <Button size='lg' className='c-386ecf ms-2 py-3 mb-2 mb-md-4'>Multijugador</Button>
+                <Button size='lg' className='c-386ecf ms-2 py-3'>Canviar estils</Button>
               </Stack>
 
             </div>
