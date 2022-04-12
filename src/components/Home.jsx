8fb4c5a84cc557bc49dd1d2dcbf36e9ceafe1491
arturmh <59830180+arturmh@users.noreply.github.com>
2022-04-12 14:45:44 +0200

    Improve home page responsive for small screen sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,19 +11,19 @@ export function Home() {
 
       <Container className='p-4 mt-5 rounded-3 bg-secondary' >
         <Row>
-          <Col xs={3}>
+          <Col xs={12} md={3}>
             <div>
               <h1 className='mb-4 mt-3 text-light text-center'>Escacs</h1>
 
-              <Stack gap={5} className="mx-auto">
-                <Button size='lg' className='ms-2 py-3' onClick={handleNav('game')}>Un Jugador</Button>
-                <Button size='lg' className='ms-2 py-3' onClick={handleNav('game')}>Multijugador</Button>
+              <Stack className="mx-auto">
+                <Button size='lg' className='ms-2 py-3 mb-2 mb-md-4 ' onClick={handleNav('game')}>Un Jugador</Button>
+                <Button size='lg' className='ms-2 py-3 mb-2 mb-md-4 ' onClick={handleNav('game')}>Multijugador</Button>
                 <Button size='lg' className='ms-2 py-3' onClick={handleNav('game')}>Canviar estils</Button>
               </Stack>
 
             </div>
           </Col>
-          <Col xs={9}>
+          <Col xs={12} md={9}>
             <Board />
           </Col>
         </Row>
